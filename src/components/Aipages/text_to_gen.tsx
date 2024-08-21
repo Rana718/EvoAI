b8f0707e3beb63c8e22382229b78971bfa1ff43a
Rana718 <ranadolui718@gmail.com>
2024-08-22 01:12:36 +0530

    Ai pages for all Type of Ai

---
+++ src/components/Aipages/text_to_gen.tsx
@@ -0,0 +1,12 @@
+import Aibar from "../Aibar"
+import InputBox from "../InputBox"
+export default function Textpage(){
+    return(
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <Aibar/>
+            <InputBox/>
+
+        </div>
+    )
+}
+
