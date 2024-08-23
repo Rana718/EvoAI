bdd97387180cf4e9eb5cb26da594466ab1e9ced9
Rana718 <ranadolui718@gmail.com>
2024-08-24 01:10:12 +0530

    ChatContainer

---
+++ src/components/ChatContainer.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+
+type Message = {
+  text: string;
+  isUser: boolean;
+};
+
+type ChatContainerProps = {
+  messages: Message[];
+};
+
+const ChatContainer: React.FC<ChatContainerProps> = ({ messages }) => {
+  return (
+    <div className="flex flex-col p-4 space-y-4 overflow-y-auto h-full bg-gray-50 rounded-lg shadow-inner">
+      {messages.map((message, index) => (
+        <div key={index} className={`flex ${message.isUser ? 'justify-end' : 'justify-start'}`}>
+          <div className={`max-w-full md:max-w-[70%] p-4 rounded-lg shadow-md transform transition-transform ${
+              message.isUser
+                ? 'bg-blue-500 text-white animate-slideInRight'
+                : 'bg-gray-200 text-black animate-slideInLeft'
+            }`}
+          >
+            <ReactMarkdown>{message.text}</ReactMarkdown>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ChatContainer;
